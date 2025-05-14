c86c67827954825df3627a34cb392df2bbd31818
SVkdf092 <97460137+SomnvK123@users.noreply.github.com>
2025-05-14 18:04:48 +0700

    Reichmarshall

--- src/main/java/com/example/security/service/UserService.java
+++ src/main/java/com/example/security/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.security.service;
 
+import com.example.security.config.jwtFilter;
 import com.example.security.dao.UserDAO;
 import com.example.security.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,6 +20,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Service
 public class UserService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     public String getCurrentUsername() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
@@ -25,9 +30,11 @@ public class UserService {
         Object principal = authentication.getPrincipal();
 
         if (principal instanceof User) {
+            logger.info("Found user: {}", principal);
             User user = (User) principal;
             return user.getUsername();
         } else if (principal instanceof UserDetails) {
+            logger.info("Found UserDetails: {}", principal);
             UserDetails userDetails = (UserDetails) principal;
             return userDetails.getUsername();
         } else {
